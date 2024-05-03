ac4ec70d7fb351fd9478c45666b06b960c772d24
Slip_Stas <kaopuk@gmail.com>
2024-05-03 10:46:51 +0300

    save

---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+engine = create_async_engine("sqlite+aiosqlite:///songs.db")
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+class Model(DeclarativeBase):
+    pass
+
+class SongsTable(Model):
+    __tablename__ = "songs"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    artist:  Mapped[str]
+    title:  Mapped[str]
+    path:  Mapped[str]
+    youtube_link: Mapped[str]
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Model.metadata.create_all)
+
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Model.metadata.drop_all)
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from typing import Annotated
-
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from contextlib import asynccontextmanager
+from database import delete_tables, create_tables
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # await delete_tables()
+    # print("database was drop!")
+    await create_tables()
+    print("create database!")
+
+    print("reload server!!!...")
+    yield
+    print("shutdown...")
+
+app = FastAPI(lifespan=lifespan)
 
 
 class SSongAdd(BaseModel):
@@ -23,7 +35,7 @@ songs_array = []
 
 @app.post("/song")
 async def add_song(song: Annotated[SSongAdd, Depends()]) -> dict:
-    song = SSong(id=1, artist="Anya Nami", title="Wake1 me up", path="data/", youtube_link="www.youtube.com/wqerty")
+    song = SSong(id=1, artist="Anya Nami", title="Wake me up", path="data/", youtube_link="www.youtube.com/wqerty")
     # song_two = SSong(id=2, artist="Anya Singltone", title="Wale up", path="data/", youtube_link="www.youtube.com/wqeyuio")
     songs_array.append(song)
 
