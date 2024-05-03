53079a4efe27a269194ee8749a8f92b40217b0b9
Slip_Stas <kaopuk@gmail.com>
2024-05-03 09:03:52 +0300

    add database

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-class Song(BaseModel):
-    id: int
+class SSongAdd(BaseModel):
     artist: str
     title: str
     path: str
     youtube_link: str
 
 
-@app.get("/songs")
-def get_songs() -> dict:
-    song_one = Song(id=1, artist="Anya Nami", title="Wake me up", path="data/", youtube_link="www.youtube.com/wqerty")
-    song_two = Song(id=2, artist="Anya Singltone", title="Wale up", path="data/", youtube_link="www.youtube.com/wqeyuio")
+class SSong(SSongAdd):
+    id: int
 
-    songs_array = [song_one, song_two]
 
-    return {"data": songs_array}
+songs_array = []
+
+
+@app.post("/song")
+async def add_song(song: Annotated[SSongAdd, Depends()]) -> dict:
+    song = SSong(id=1, artist="Anya Nami", title="Wake1 me up", path="data/", youtube_link="www.youtube.com/wqerty")
+    # song_two = SSong(id=2, artist="Anya Singltone", title="Wale up", path="data/", youtube_link="www.youtube.com/wqeyuio")
+    songs_array.append(song)
+
+    return {"result": "ok",
+            "song_id": song.id}
+
+
+# @app.get("/songs")
+# def get_songs() -> dict:
+#     song_one = SSong(id=1, artist="Anya Nami", title="Wake me up", path="data/", youtube_link="www.youtube.com/wqerty")
+#     song_two = SSong(id=2, artist="Anya Singltone", title="Wale up", path="data/", youtube_link="www.youtube.com/wqeyuio")
+#
+#     songs_array = [song_one, song_two]
+#
+#     return {"data": songs_array}
 
